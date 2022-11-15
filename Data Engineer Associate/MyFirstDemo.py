6efcf8a358f843162c685e79c177433c98989357
vsekar602@gmail.com <vsekar602@gmail.com>
2022-11-15 05:07:50 +0000

    practicefiles

--- Data Engineer Associate/MyFirstDemo.py
+++ Data Engineer Associate/MyFirstDemo.py
@@ -1,2 +1,82 @@
 # Databricks notebook source
+import pandas as pd
+
+# COMMAND ----------
+
+pd.__version__
+
+# COMMAND ----------
+
+lst = [1,2,3,4,5]
+
+def identify_even(lst):
+    for i in lst:
+        if i%2!=0:
+            return i
+        else:
+            return "No Number"
+
+# COMMAND ----------
+
+lst = [1,2,3,4,5]
+even= identify_even(lst)
+
+# COMMAND ----------
+
+print(even)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
 
